35ead86efd86c1f3b2eff1ccc8a889e0a680d5a8
Ethan600 <77286763+Ethan600@users.noreply.github.com>
2022-01-24 19:41:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function setup(){
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function preload(){
+    classifier = ml5.imageClassifier("DoodleNet");
+}
+
+function classifyCanvas(){
+    classifier.classify(canvas, gotResults);
+}
+
+function draw(){
+    strokeWeight(10);
+    stroke("blue");
+    if (mouseIsPressed){
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+
+function gotResults(error, result){
+    if(error){
+        console.error(error);
+    }
+
+    else{
+        console.log(result);
+        document.getElementById("label").innerHTML = "Label:" + result[0].label;
+        document.getElementById("confidence").innerHTML = "Confidence:" + Math.round(result[0].confidence * 100) + + "%";
+        utterThis = new SpeechSynthesisUtterance(result[0].label);
+        synth.speak(utterThis);
+    }
+}
+
+function clearCanvas(){
+    background("white");
+}
